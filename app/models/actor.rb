18ced0143fa4b6cad0f919ec4acd3ad7225756f0
Lisa Lee <snickers495@gmail.com>
2017-10-30 22:57:23 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     characters.each do |character|
-      puts "#{character} #{character.show}"
+      puts "#{character.name} - #{character.show}"
     end
-  end 
+  end
 end

215af86e6a2f3b85c55fe46b3d92dd4c39902500
Lisa Lee <snickers495@gmail.com>
2017-10-30 22:53:23 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 8) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 8) do
   end
 
   create_table "characters", force: :cascade do |t|
-    t.string "catchphrase"
-    t.string "name"
+    t.string  "name"
+    t.integer "show_id"
+    t.string  "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
